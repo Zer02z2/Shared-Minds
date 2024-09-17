2d24a10c845a9cb21a8f1d505f118cd301069395
Zer02z2 <zongzechan@gmail.com>
2024-09-16 22:55:25 -0400

    Update

--- src/week2/sketch.ts
+++ src/week2/sketch.ts
@@ -1,3 +1,39 @@
-import { secrets } from "../secrets"
+import { getElementPosition, Random } from "../library"
 
-console.log(secrets.replicateProxy)
+const canvas = document.getElementById("canvas") as HTMLCanvasElement
+const ctx = canvas.getContext("2d")
+const inputContainer = document.getElementById("input-container") as HTMLElement
+const input = document.getElementById("input-field") as HTMLInputElement
+
+const init = (): void => {
+  if (!(ctx && input)) return
+
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+  let [inputX, inputY] = getElementPosition(inputContainer)
+  const fontSize = 30
+
+  const render = (): void => {
+    ctx.fillStyle = "rgba(150,150,150,0.9)"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.font = `${fontSize}px Helvetica`
+  }
+
+  render()
+
+  input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      const value = input.value
+      const words = value.split(/[^a-zA-Z]+/).filter(Boolean)
+
+      words.forEach((word) => {})
+      render()
+
+      input.value = ""
+      inputContainer.style.left = `${inputX}px`
+      inputContainer.style.top = `${inputY}px`
+    }
+  })
+}
+
+init()
